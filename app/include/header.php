753ae35c5c9c3b4c33ec29cc7f6aa32e18a94dd4
dimonstr223 <dimstukmikolu@gmail.com>
2023-03-17 05:54:30 +0300

    logout

--- app/include/header.php
+++ app/include/header.php
@@ -5,12 +5,10 @@
 	<meta charset="utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<title>My blog</title>
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
-		integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous" />
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous" />
 	<link rel="preconnect" href="https://fonts.googleapis.com" />
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-	<link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;400;500;600;700&display=swap"
-		rel="stylesheet" />
+	<link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
 	<link rel="stylesheet" href="/assets/css/style.css" />
 </head>
 
@@ -32,25 +30,25 @@
 						<li>
 							<? if (isset($_SESSION['id'])) : ?>
 
-							<a href="#"><i class="icon fa-solid fa-user"></i>
-								<?= $_SESSION['username'] ?>
-							</a>
-							<ul>
-								<? if ($_SESSION['admin']) : ?>
-								<li><a href="<?php echo BASE_URL . 'admin/admin.php' ?>">Админ панель</a></li>
-								<? endif; ?>
+								<a href="#"><i class="icon fa-solid fa-user"></i>
+									<?= $_SESSION['username'] ?>
+								</a>
+								<ul>
+									<? if ($_SESSION['admin']) : ?>
+										<li><a href="<?php echo BASE_URL . 'admin/admin.php' ?>">Админ панель</a></li>
+									<? endif; ?>
 
-								<li><a href="#">Выход</a></li>
-							</ul>
+									<li><a href="<?= BASE_URL . 'logout.php' ?>">Выход</a></li>
+								</ul>
 
 							<? else : ?>
 
-							<a href="<?= BASE_URL . 'login.php' ?>"><i class="icon fa-solid fa-user"></i>
-								Вход
-							</a>
-							<ul>
-								<li><a href="<?= BASE_URL . 'signup.php' ?>">Регистрация</a></li>
-							</ul>
+								<a href="<?= BASE_URL . 'login.php' ?>"><i class="icon fa-solid fa-user"></i>
+									Вход
+								</a>
+								<ul>
+									<li><a href="<?= BASE_URL . 'signup.php' ?>">Регистрация</a></li>
+								</ul>
 
 							<? endif; ?>
 						</li>
