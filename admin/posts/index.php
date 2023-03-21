e2c5f97951d097b7908f5d589c0d0cab0fc55277
dimonstr223 <dimstukmikolu@gmail.com>
2023-03-21 16:58:46 +0300

    вывод статей в админку

--- admin/posts/index.php
+++ admin/posts/index.php
@@ -15,18 +15,25 @@ include '../../app/include/header-admin.php';
 				<div class="row table-title">
 					<div class="table-title__item id col-1">ID</div>
 					<div class="table-title__item name col-5">Название</div>
-					<div class="table-title__item author col-2">Автор</div>
-					<div class="table-title__item edit col-2 ">Редакт.</div>
-					<div class="table-title__item delete col-2 ">Удалить</div>
+					<div class="table-title__item author col-3">Автор</div>
+					<div class="table-title__item edit col-3 ">Редактировать</div>
 				</div>
 				<div class="items">
 					<?php foreach ($posts as $key => $value) : ?>
 						<div class="row post">
 							<div class=" post__item id col-1"><?= $key + 1; ?></div>
 							<div class=" post__item name col-5"><?= $value['title'] ?></div>
-							<div class=" post__item author col-2">Димонстр</div>
-							<div class=" post__item edit col-2 col-green"><a href="#">Edit</a></div>
-							<div class=" post__item delete col-2 col-red"><a href="#">Delete</a></div>
+							<div class=" post__item author col-3"><?= $value['id_user'] ?></div>
+							<div class=" post__item edit col-1 col-green"><a href="#">Edit</a></div>
+							<div class=" post__item delete col-1 col-red"><a href="#">Delete</a></div>
+							<div class=" post__item delete col-1 d-flex">
+								<label for="<?= $value['id'] ?>">Public</label>
+								<?php if ($value['status']) : ?>
+									<input type="checkbox" checked name="publick" id="<?= $value['id'] ?>">
+								<?php else : ?>
+									<input type="checkbox" name="publick" id="<?= $value['id'] ?>">
+								<? endif ?>
+							</div>
 						</div>
 					<? endforeach ?>
 				</div>
